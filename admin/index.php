3bfd27132e57be9a546de9a2910c772caa2c0fb3
Fernando VI. <f3rnandogaff@gmail.com>
2025-08-22 01:00:33 -0600

    Añadiendo la visualización rn PHP los registros de la BD

--- admin/index.php
+++ admin/index.php
@@ -16,9 +16,36 @@
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <!--Creamos una tabla-->
+
+        <table class="propiedades">
+            <thead>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img class="imagen-tabla" src="/imagenes/2dfcab6228fcc16e519bcc9d1e0abfd5.jpg" alt=""></td>
+                    <td>$12000000</td>
+                    <td>
+                        <a class="boton-rojo-block" href="#">Eliminar</a>
+                        <a class="boton-amarillo-block" href="#">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
     </main>
 
 <?php
     incluirTemplate('footer');
 
 ?>
+
